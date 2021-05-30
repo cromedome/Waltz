8e1850f8b16b0dd9aaff83a02fd9b91f99b6b29c
Jason A. Crome <jason@crome-plated.com>
2021-05-29 22:16:46 -0400

    Build the skeleton of the CLI interface.

---
+++ lib/Waltz/CLI.pm
@@ -0,0 +1,35 @@
+package Waltz::CLI;
+
+use Moo;
+use CLI::Osprey;
+use File::Share 'dist_dir';
+use Module::Runtime 'use_module';
+
+# TODO: All of these
+subcommand version   => 'Waltz::CLI::Version';
+#subcommand validate  => 'Waltz::CLI::Validate';
+#subcommand search    => 'Waltz::CLI::Search';
+#subcommand site      => 'Waltz::CLI::Site';
+#subcommand page      => 'Waltz::CLI::Page';
+#subcommand publish   => 'Waltz::CLI::Publish';
+#subcommand dev       => 'Waltz::CLI::Dev';
+#subcommand bootstrap => 'Waltz::CLI::Bootstrap';
+
+# Thinking ahead, these might be useful in future subcommands
+has _waltz_version => (
+    is      => 'lazy',
+    builder => sub { use_module( 'Waltz' )->VERSION },
+);
+
+has _dist_dir => (
+    is      => 'lazy',
+    builder => sub{ dist_dir('Waltz') },
+);
+
+sub run {
+    my $self = shift;
+    return $self->osprey_usage;
+}
+
+1;
+
