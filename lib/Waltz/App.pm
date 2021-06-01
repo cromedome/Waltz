9d3f168747ab0675db1f52d4437b47a24b76130d
Jason A. Crome <jason@crome-plated.com>
2021-06-01 14:55:01 -0400

    WIP: Dancer2 config issues.
    Either I am not using the ENV variable overrides correctly, or
    Dancer2 config is way more broken than I am giving it credit for. In
    either case, my code is *clearly* not working as intended.
    
    Setting the environment variables does not seem to alter the
    configuration of Dancer2 at all. This can't be correct. What am I
    messing up?

--- lib/Waltz/App.pm
+++ lib/Waltz/App.pm
@@ -10,21 +10,31 @@ use Text::Markdown 'markdown';
 use Waltz;
 use Data::Printer;
 
-# The goal here is to pigeon hole us into our site content
-# directory. I am not sure yet about letting a user spefify
-# what directory they want content to come from. Seems a 
-# bit dangerous. 
-my $cwd = getcwd;
-set views => "$cwd/views";
+# The goal here is to pigeon hole us into our site content directory. I am not
+# sure yet about letting a user spefify what directory they want content to
+# come from. Seems a bit dangerous. 
+#BEGIN {
+    #my $cwd = getcwd; 
+    #debug "CWD IS $cwd";
+    
+    #$ENV{ DANCER_CONFDIR } = $cwd;
+    #$ENV{ DANCER_VIEWS   } = "$cwd/views";
 
-# This needs to change when we go to publish. Then again,
-# maybe not. TBD.
-set public_dir => "$cwd/static";
+    ## This needs to change when we go to publish. Then again, maybe not. TBD.
+    #$ENV{ DANCER_PUBLIC } = "$cwd/static";
+#}
 
-# This seems good for development, not sure if it will
-# stick around for the long haul.
-get '/version' => sub {
-    return "Version " . $Waltz::VERSION;
+# This seems good for development, not sure if it will stick around for the
+# long haul.
+get '/version' => sub { return 'Version ' . $Waltz::VERSION; };
+
+get '/settings' => sub {
+    debug "VIEWS ARE " . $ENV{ DANCER_VIEWS };
+    my @views = setting( 'views' ); p @views;
+    my @public = setting( 'public_dir' ); p @public;
+    return 'Settings:<br>' .
+        'Views: ' . setting 'views',
+        'Static dir: ' . setting 'public_dir';
 };
 
 get '/' => sub {
